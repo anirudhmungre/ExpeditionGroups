e6da5e4b4b6897be67b1fa323c429ae06c1c1e71
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-15 17:34:52 -0500

    implement graph structure

---
+++ Graph.js
@@ -0,0 +1,14 @@
+class Graph {  
+    constructor(numVertices){
+        this.numVertices = numVertices 
+        this.AdjList = new Map()
+    }
+    addVertex = (v) => { 
+        this.AdjList.set(v, []); 
+    } 
+    addEdge = (v, w) => {
+        this.AdjList.get(v).push(w)
+        // Undirected graph so must add opposite edge
+        this.AdjList.get(w).push(v)  
+    }
+} 
