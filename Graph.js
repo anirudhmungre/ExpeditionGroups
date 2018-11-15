3acd6429e8c932b4a4652fc89326416f9c8c9842
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-15 18:40:28 -0500

    fix structure

--- Graph.js
+++ Graph.js
@@ -1,10 +1,11 @@
 export class Graph {  
     constructor(numVertices){
-        this.numVertices = numVertices 
+        this.numVertices = numVertices
         this.AdjList = new Map()
     }
     addVertex = (v) => { 
-        this.AdjList.set(v, []); 
+        this.AdjList.set(v, null)
+        this.numVertices++ 
     } 
     addEdge = (v, w) => {
         this.AdjList.get(v).push(w)
